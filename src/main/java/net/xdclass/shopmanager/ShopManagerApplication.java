8cc7bf53cc83010b46157cfe6e1286d171a95ba9
406766467 <406766467@qq.com>
2021-01-26 15:46:56 +0800

    修改1

--- src/main/java/net/xdclass/shopmanager/ShopManagerApplication.java
+++ src/main/java/net/xdclass/shopmanager/ShopManagerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopManagerApplication {
 
     public static void main(String[] args) {
+        System.out.println("begin");
         SpringApplication.run(ShopManagerApplication.class, args);
     }
 
